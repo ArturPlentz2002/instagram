e3e7d36aa76ade30d43947825721a8d26b2840da
Artur Plentz <artursplentz@gmail.com>
2025-02-23 20:13:10 -0300

    bugs

---
+++ src/actions.ts
@@ -0,0 +1,26 @@
+"user server"
+
+import { PrismaClient } from "@prisma/client"
+
+import { User } from "@prisma/client"
+
+import path from "path"
+
+const prisma = new PrismaClient();
+
+//Recupera o usuário pelo email
+
+export async function getUserByEmail( 
+    email: string | null
+): Promise<User | null> {
+    if (!email) return null;
+
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email: email,
+        },
+    });
+
+    return user;
+}
