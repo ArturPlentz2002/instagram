1ddb9000f5fe71cbbc43cbc6c719f868e8e23d6f
Artur Plentz <artursplentz@gmail.com>
2025-02-23 20:47:06 -0300

    start another button buttonLink.tsx

--- src/components/ButtonProps.tsx
+++ src/components/ButtonProps.tsx
@@ -8,6 +8,7 @@ type ButtonProps = ComponentProps<"button"> & {
 const Button: React.FC<ButtonProps> = ({ text, danger = false, ...props }) => {
   return (
     <button
+    //sobreescrever a cor do botão pelo tailwind pelo danger
       data-danger={danger}
       className="h-8 text-sm font-medium text-white py-1 px-6 rounded flex items-center bg-blue-800 hover:bg-blue-700 data-[danger=true]:bg-red-500 data-[danger=true]:hover:bg-red-400"
       {...props}
