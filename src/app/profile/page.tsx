76c1ef3a1fc137edf21dffa95221bea7cae51330
Artur Plentz <artursplentz@gmail.com>
2025-02-24 09:06:04 -0300

    perfil

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,41 @@
+import { getUserByEmail } from "@/actions";
+import ProfileForm from "@/components/ProfileForm";
+import { auth } from "auth";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
+export default async function UserProfile() {
+  const session = await auth();
 
-const ProfilePage = () => {
+  // Não tem session, vai para home
+  if (!session || !session.user?.email) return redirect(`/`);
+
+  const user = await getUserByEmail(session.user.email);
+
+  // Não tem usuário vai para home, se estiver logado volta para home
+  if (!user) {
+    return redirect(`/`);
+  }
+
   return (
-    <div>ProfilePage</div>
-  )
-}
+    <div className="w-[35rem] mx-auto my-10 p-4">
+      <h1 className="text-[2rem] leading-10 font-semibold text-center">
+        Perfil de {user.name}
+      </h1>
 
-export default ProfilePage;
+      {user.image && (
+        <div className="w-full flex justify-center my-6">
+          <Image
+            src={user.image}
+            alt={`Imagem de perfil de ${user.name}`}
+            className="w-80 h-80 p-4 object-cover"
+            width={320}
+            height={320}
+          />
+        </div>
+      )}
+
+      <ProfileForm user={user} />
+    </div>
+  );
+}
